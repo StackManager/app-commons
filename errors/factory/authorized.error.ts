ea8a1575ef1b4c53c0dec55a0b8197867e1f70b5
aruiz <aruiz@globaltask.net>
2024-07-10 11:11:59 -0400

    feat(errors): Improvements details notification

--- errors/factory/authorized.error.ts
+++ errors/factory/authorized.error.ts
@@ -2,17 +2,17 @@ import { ModelErrorBase } from '../error.base';
 
 export class NotAuthorizedError extends ModelErrorBase {
   statusCode = 401;
-
-  constructor(error: unknown = undefined) {
+  myDetail: string | undefined = ''
+  constructor(error: string | undefined = undefined) {
     super('Not Authorized', error);
-
+    this.myDetail = error
     Object.setPrototypeOf(this, NotAuthorizedError.prototype);
   }
 
   serializeErrors() {
     return [{ 
       message: 'Not authorized', 
-      detail: this.detail 
+      detail: this.myDetail
     }];
   }
 }
